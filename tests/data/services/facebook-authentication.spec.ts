686fda3a72aa48ed9a520fbfd2e48ff77419e84d
Deivid Breno Novaes <dbrenoti@gmail.com>
2022-07-25 17:23:41 -0300

    Move FacebookAuthenticationService to its own file

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,18 +1,6 @@
 import { AuthenticationError } from '@/domain/errors'
-import { LoadFacebookUserApi } from '@/domain/contracts/apis'
-import { FacebookAuthentication } from '@/domain/features'
-
-export class FacebookAuthenticationService {
-  constructor (
-    private readonly loadFacebookUserApi: LoadFacebookUserApi
-  ) {}
-
-  async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
-    await this.loadFacebookUserApi.loadUser(params)
-
-    return new AuthenticationError()
-  }
-}
+import { LoadFacebookUserApi } from '@/data/contracts/apis'
+import { FacebookAuthenticationService } from '@/data/services'
 
 class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
   public token?: string
